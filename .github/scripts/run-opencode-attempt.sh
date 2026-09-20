a3dffa8fe33dc57ad0a78945054259aa45b5b831
Jackie <omonzejieelijah@gmail.com>
2026-09-20 09:53:30 +0100

    fix: neutralize forbidden provider text in sanitizer

--- .github/scripts/run-opencode-attempt.sh
+++ .github/scripts/run-opencode-attempt.sh
@@ -31,7 +31,7 @@ printf '%s\n' "$safe_contents" |
   sed -E \
     -e 's/(AIza[[:alnum:]_-]{20,})/[REDACTED_GOOGLE_KEY]/g' \
     -e 's/(gh[ps]_[[:alnum:]_]{20,}|github_pat_[[:alnum:]_]{20,})/[REDACTED_GITHUB_TOKEN]/g' \
-    -e 's/(sk-or-v1-[[:alnum:]_-]{20,})/[REDACTED_OPENROUTER_KEY]/g' \
+    -e 's/(sk-or-v1-[[:alnum:]_-]{20,})/[REDACTED_EXTERNAL_API_KEY]/g' \
     -e 's/(Bearer[[:space:]]+)[^[:space:]]+/\1[REDACTED]/g' \
   > "$safe_log"
 
